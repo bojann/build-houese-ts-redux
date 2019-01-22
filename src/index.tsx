815ae89f5f3b6e2881304c248b18cf092b6e1a8e
b.arcon <b.arcon@samsung.com>
2019-01-22 13:06:41 +0100

    init app

--- src/index.tsx
+++ src/index.tsx
@@ -2,9 +2,32 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
+// import { Provider } from 'react-redux';
+// import configureStore from 'state/store';
 import * as serviceWorker from './serviceWorker';
+import { BrowserRouter} from 'react-router-dom';
 
 ReactDOM.render(<App />, document.getElementById('root'));
+
+const render = (Component: React.ComponentType)=> {
+  return ReactDOM.render(
+    <BrowserRouter>
+      <Component />
+    </BrowserRouter>,
+    document.getElementById('root')
+  );
+};
+
+render(App);
+
+declare const module: any;
+
+if (module.hot) {
+  module.hot.accept('./App', () => {
+    const NextApp = require('./App').default;
+    render(NextApp);
+  });
+}
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
